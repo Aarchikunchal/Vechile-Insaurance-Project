022e877ee0c25b3ec84f9a7065b4eb98d782143c
Aarchi <kunchalaarchi@gmail.com>
2025-11-20 09:52:52 +0530

    project structure ready , venv setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup , find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    author = "Aarchi kunchal",
+    author_email="kunchalaarchi@gmail.com",
+    packages = find_packages()
+
+)
